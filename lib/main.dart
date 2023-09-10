23e5e675a7e12abaab2f0e883910900e5e7f5fec
Vivek <0126ds201063@oriental.ac.in>
2023-09-10 22:27:53 +0530

    done

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:quotes/home_screen.dart';
+
+void main() {
+  runApp(const QuotesApp());
+}
+
+class QuotesApp extends StatelessWidget {
+  const QuotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
